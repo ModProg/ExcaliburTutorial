a7f130ec4f1a9601df672f019f7371bf49f713f9
Roland Fredenhagen <modprog@freenet.de>
2019-07-06 11:05:19 +0200

    Tutorial 0 end

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,9 @@ var game = new ex.Engine({
   displayMode: ex.DisplayMode.FullScreen
 })
 
+game.backgroundColor = ex.Color.fromRGB(0,150,100);
+var helloWorld = new ex.Label("Hello World",200,200);
+game.add(helloWorld);
+helloWorld.fontSize = 100;
 // Starten der Engine
 game.start()
