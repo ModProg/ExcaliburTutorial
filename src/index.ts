05a38a92c06bedf74bf193ccf857526448924241
ModProg <roland@van-fredenhagen.de>
2019-07-06 17:50:24 +0200

    Tutorial 1 end

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
 import * as ex from 'excalibur';
 var game = new ex.Engine({
   // Stellt den Darstellungsmodus auf Fullscreen
-  displayMode: ex.DisplayMode.FullScreen
+  displayMode: ex.DisplayMode.FullScreen,
+  backgroundColor: ex.Color.fromRGB(10, 100, 50)
+})
+var crosshair = new ex.Label({
+  text: '⊕',
+  x: game.canvasWidth / 2,
+  y: game.canvasHeight / 2,
+  fontSize: 150,
+  textAlign: ex.TextAlign.Center,
+  baseAlign: ex.BaseAlign.Middle
 })
 
-game.backgroundColor = ex.Color.fromRGB(0,150,100);
-var helloWorld = new ex.Label("Hello World",200,200);
-game.add(helloWorld);
-helloWorld.fontSize = 100;
+game.add(crosshair);
+game.canvas.style.cursor = 'none'
+game.input.pointers.primary.on('move', function (evt) {
+  crosshair.pos = evt.target.lastWorldPos
+})
+game.input.pointers.primary.on('down', function (evt) {
+  game.add(new ex.Actor({
+    x: evt.target.lastWorldPos.x,
+    //-10 um die Box ein Stück hochzubewegen, damit sie besser passt
+    y: evt.target.lastWorldPos.y - 15,
+    width: 50,
+    height: 50,
+    color: ex.Color.White
+  }))
+})
 // Starten der Engine
 game.start()
